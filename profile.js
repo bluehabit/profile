22538876f5d11288bdc91368e4c308d1c11ab763
Chris Feltus <chris.feltus@gmail.com>
2017-01-15 09:33:08 -0600

    profile and video

---
+++ profile.js
@@ -0,0 +1,56 @@
+{
+      "firstName": "Chris",
+      "lastName": "Feltus",
+      "about": "Left and right brain hemisphere enthusiast.",
+      "portfolio": null,
+      // evidentialDegree Options: "Adv" "Master" "Elite"
+      "evidentialDegree": "Master",
+      "quote": "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+      "quoteSource": "Aristotle",
+      "location": {
+        "city": "Arlington",
+        "state": "Texas",
+        "country": "USA"
+      },
+      "photos": [
+        "neon80sbirb.mp4",
+      ],
+      "publishedArticles": [
+        {
+          "tile": "Article Title",
+          "publishedDated": "",
+          "link": "",
+          // "role" for publishedArticles can be "author" or "coauthor"
+
+          "role": "author"
+        }
+      ],
+      "pinnacleProject": {
+        "title": "",
+        "completionDate": "",
+        "problemSolving": "",
+        "humanityImpact": "",
+        "link": ""
+      },
+      "careerPaths": [
+        {
+          // careerPathID Options: "careerPath_1" "careerPath_2" "careerPath_3" "careerPath_3" "careerPath_3E" "careerPath_5E"
+          "careerPathID": "careerPath_3",
+          "careerPathProgress": {
+            "domainKnowledge": 10,
+            "uiDevSkills": 20,
+            "coursesCompleted": [
+              {
+                // For courseID, we will provide a list of all the courses to help you fill this out
+                "courseID": "course_1",
+                "dateCompleted": ""
+              },
+              {
+                "courseID": "course_2",
+                "dateCompleted": ""
+              }
+            ]
+          }
+        }
+      ]
+    }
